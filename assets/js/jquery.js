6c27464cd95aabc5bfc28995dc6bc09ca8fce1f2
Alejandro Silva Quiñones <janosilva@gmail.com>
2024-02-01 19:00:41 -0300

    Add files via upload
    Ajustes en HTML, CSS y JS, renombre de cloning por index y cumplimiento de 4 de 5 puntos solicitados

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,10 +1,40 @@
-$("button").click(function(){
-    $("card").toggle("slow");
+$("#button1").on( "click", function() {
+    $(".card").toggle("slow");
+  });
+$("#button2").on( "click", function() {
+    $(".card").toggle("slow");
+  });
+$("#button3").on( "click", function() {
+    $(".card").toggle("slow");
   });
 
-  $( "#jq-title" ).on( "dblclick", function() {
+
+$( "#jq-title" ).on( "dblclick", function() {
+    $(this).css({ 
+    "color": "red",    
+    });
+});
+$( "#jq-title2" ).on( "dblclick", function() {
     $(this).css({ 
     "color": "red",    
     });
 });
 
+$("#mailsend").on( "click", function() {
+  alert( "El correo fue enviado correctamente..." );
+});
+
+$('#mover').on( "mouseenter", function() {
+  $('body').append("<div id='hoveringTooltip' style='position:fixed;'></div>");
+  $('#hoveringTooltip').html("Haz click y la receta llegara a tu correo en unos momentos");
+  $('#hoveringTooltip').css({
+      "top" : $(this).offset().top + MYTOPOFFSET,
+      "left" : $(this).offset().left + MYLEFTOFFSET
+  });
+});
+$('#mover').on( "mouseenter", function() {
+  $('#hoveringTooltip').remove();
+});
+$(function() {
+  $( '.mover[title]' ).tooltip();
+});
